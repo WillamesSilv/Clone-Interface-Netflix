f996ff3f6c932f5867cd15ae745e77504483a4d8
WillamesSilv <willamessantos@live.com>
2021-09-12 22:05:56 -0300

    finalizing featured film

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -26,8 +26,8 @@ export default ({item}) => {
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`}>Assistir</a>
-                        <a href={`/list/add/${item.id}`}>Minha Lista</a>
+                        <a href={`/watch/${item.id}`} className="featured--watchButton">Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--myListButton">Minha Lista</a>
 
                     </div>
                     <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
